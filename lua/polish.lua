2784013f9947c863b57d0e3466054add59ba4d39
Sergey Naumov <drmgc@yandex.ru>
2024-05-16 11:29:32 +0500

    configure spelling

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,4 +2,8 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+vim.opt.spelllang = "en_us,ru"
+vim.opt.spelloptions = "camel"
+vim.opt.spell = true
+
 require("lspconfig").gdscript.setup { cmd = { "ncat", "localhost", "6005" } }
